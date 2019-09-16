4ab8826bf6061fe242fdb040180478624d0881fc
Hanno Vahtrik <hannovahtrik@hotmail.com>
2019-09-16 20:03:50 +0300

    Add project files.

---
+++ RikTest/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RikTest
+{
+    public class Class1
+    {
+    }
+}
