b0f41e32b558d6f91a73efa7d1acb4680a311824
Hanno Vahtrik <hannovahtrik@hotmail.com>
2019-09-18 22:30:09 +0300

    SearcTest

---
+++ RikTest/TestCases/SearchTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace RikTest.TestCases
+{
+    class SearchTest
+    {
+        [Test]
+        public void Test()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://transport.tallinn.ee/#tallinna-linn/map";
+
+            driver.FindElement(By.CssSelector("input[type='text']")).SendKeys("Tramm number 66");
+            bool element = driver.FindElement(By.CssSelector(".stop")).Displayed;
+            Assert.IsTrue(element);
+            driver.Quit();
+
+        }
+    }
+}
